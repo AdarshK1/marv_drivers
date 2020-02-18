bdd4167402dadc9e14ea2837d691da2963b07463
Adarsh <akulkarn@seas.upenn.edu>
2020-02-18 17:53:44 -0500

    merged

--- arduino/arduino_telemetry.py
+++ arduino/arduino_telemetry.py
@@ -1,15 +1,34 @@
-from time import sleep
-import serial
-ser = serial.Serial('/dev/tty.usbmodem1462201', 115200) # Establish the connection on a specific port
-counter = 32 # Below 32 everything in ASCII is gibberish
-while True:
-     # counter +=1
-     # ser.write(str(chr(counter))) # Convert the decimal number to ASCII then send it to the Arduino
-     ser.write(str.encode('a'))
+#!/usr/bin/python3
 
-     print("From laptop")
-     print (ser.readline()) # Read the newest output from the Arduino
+import rospy
+from geometry_msgs.msg import PoseStamped
+from sensor_msgs.msg import Imu
+from time import sleep, time
+import serial
+
+ser = serial.Serial('/dev/ttyACM0', 115200) # Establish the connection on a specific port
+
+pub_compass = rospy.Publisher("/arduino_telemtry/compass_orientation", PoseStamped, queue_size=1)
+pub_accel = rospy.Publisher("/arduino_telemtry/compass_imu", Imu, queue_size=1)
+
+rospy.init_node("compass_pub")
+
+while not rospy.is_shutdown():
+
+     telem_array = str(ser.readline()).split(",")[1:-1] 
+
+     if len(telem_array) != 6:
+         continue
+
+     telem_array[0] = telem_array[0].split(":")[1].strip()
      
-     sleep(.1) # Delay for one tenth of a second
-     # if counter == 255:
-     # 	counter = 32 
+     pose_msg = PoseStamped()
+     pose_msg.header.frame_id = "earth"
+     pose_msg.header.stamp = rospy.Time.now()
+     pose_msg.pose.orientation.x = float(telem_array[2])
+     pose_msg.pose.orientation.y = float(telem_array[1])
+     pose_msg.pose.orientation.z = float(telem_array[0])
+     pub_compass.publish(pose_msg)
+     print(telem_array)
+
+     sleep(.1) 
